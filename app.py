1fac27e6726647d66101718c9ad51b8739cecd1b
MarviBrohi <balochmarvi283@gmail.com>
2025-04-15 12:21:57 +0500

    chnaged title

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pickle
-st.title("Salary Prediction")
+st.title("Marvi Baloch")
 with open("model.pkl","rb") as file:
     model = pickle.load(file)
 user_input = st.number_input("Experience years", min_value=0.0, max_value=50.0, value=0.0, step=0.1)
