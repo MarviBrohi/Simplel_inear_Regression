a43cc1f472325e44fd853f836c989582e8e26426
MarviBrohi <balochmarvi283@gmail.com>
2025-04-15 12:28:08 +0500

    changed title

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.title("Marvi Baloch")
 with open("model.pkl","rb") as file:
     model = pickle.load(file)
 user_input = st.number_input("Experience years", min_value=0.0, max_value=50.0, value=0.0, step=0.1)
-
-st.success(model.predict([[user_input]]))
+if st.button("Predict Salary"):
+    st.success(model.predict([[user_input]]))
